7803ba39f8226431655a56f241ed573051b94d9d
Luis Miguel <ldelafuentefelipa@gmail.com>
2025-11-10 00:29:57 -0500

    Estructura de HomeController para las paginas

--- src/main/java/com/dragontech/dragontech/controller/HomeController.java
+++ src/main/java/com/dragontech/dragontech/controller/HomeController.java
@@ -1,5 +1,45 @@
 package com.dragontech.dragontech.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@Controller
 public class HomeController {
 
+    @GetMapping("/")
+    public String home(){
+        return "index";
+    }
+
+    @GetMapping("/nosotros")
+    public String nosotros() {
+        return "nosotros";
+    }
+    
+    @GetMapping("/servicios")
+    public String servicios() {
+        return "servicios";
+    }
+
+    @GetMapping("/proyectos")
+    public String proyectos() {
+        return "proyectos";
+    }
+
+    @GetMapping("/clientes")
+    public String clientes() {
+        return "clientes";
+    }
+
+    @GetMapping("/blog")
+    public String blog() {
+        return "blog";
+    }
+
+    @GetMapping("/contacto")
+    public String contacto() {
+        return "contacto";
+    }
 }
